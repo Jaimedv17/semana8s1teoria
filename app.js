3f4961c339f1884de8d63f789c62e2e070dc0a84
Jaimedv17 <jaime.durand.vega@gmail.com>
2023-09-05 14:39:16 -0500

    ejercicio

---
+++ app.js
@@ -0,0 +1,27 @@
+
+// TIPOS DE DATOS
+//(let)
+
+//number
+let number3 = 30;
+console.log (number3);
+
+//string: cadena de texto o letras
+let lastName = "Jaime"
+console.log (lastName)
+
+//boolean: verdad o falso
+let isLogged = false;
+console.log (isLogged);
+
+//undefined: significa que la variable se a creado pero no se a definido
+let agePerson;
+console.log (agePerson)
+
+//null: es vacio
+let amptySate = null;
+console.log (amptySate);
+
+
+
+
